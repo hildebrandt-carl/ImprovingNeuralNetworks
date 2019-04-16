3888c0dbce04472939de3e637a2547ac22c4243e
Carl Hildebrandt <hildebrandt.carl@outlook.com>
2019-04-16 17:05:05 -0400

    Added the new networks

---
+++ src/Net-Gen/experiment1.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+python3 small_dense.py
+python3 large_dense.py
+python3 conv.py
---
+++ src/Net-Gen/experiment2.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+python3 N1.py
+python3 N2.py
+python3 N3.py
+python3 N4.py
+python3 N5.py
+python3 N6.py
+python3 N7.py
+python3 N8.py
+python3 N9.py
